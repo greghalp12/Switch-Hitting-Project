d19276b766a7b16ebdff1b1b8495e1a0d5bf8c4a
greghalp12 <GregoryHalperin2024@u.northwestern.edu>
2024-07-23 13:44:54 -0400

    Update Final Project - Switch Hitting.R

--- Final Project - Switch Hitting.R
+++ Final Project - Switch Hitting.R
@@ -23,8 +23,27 @@ SH_vs_LHP <- read_excel("2018-2023 Switch-Hitter vs LHP as RHH.xlsx")
 SH_vs_RHP <- read_excel("2018-2023 Switch-Hitter vs RHP as LHH.xlsx")
 
 #subsetting switch-hitters, totaling PAs
-Martetotal <- left_join(SH_vs_LHP, SH_vs_RHP, by = (PlayerId == 13613))
+SHtotals <- left_join(SH_vs_LHP, SH_vs_RHP, by = 'PlayerId')
 
-#subsetting switch-hitter data by pitcher handedness
-MartevsL <- filter(SH_vs_LHP, PlayerId == 13613)
+league_totals <- left_join(League_vs_LHP, League_vs_RHP, by = 'Season')
+
+league_av <- data.frame(Name = "Johnny Wholestaff",
+                        Pitcher_Handedness.L = "Left",
+                        PA.L = sum(league_totals$PA.x),
+                        wOBA.L = round(mean(league_totals$wOBA.x),3),
+                        OPS.L = round(mean(league_totals$OPS.x),3),
+                        "GB%.L" = round(mean(league_totals$`GB%.x`),3),
+                        "LD%.L" = round(mean(league_totals$`LD%.x`),3),
+                        "Hard%.L" = round(mean(league_totals$`Hard%.x`),3),
+                        "BB%.L" = round(mean(league_totals$`BB%.x`),3),
+                        "K%.L" = round(mean(league_totals$`K%.x`),3),
+                        Pitcher_Handedness.R = "Right",
+                        PA.R = sum(league_totals$PA.y),
+                        wOBA.R = round(mean(league_totals$wOBA.y),3),
+                        OPS.R = round(mean(league_totals$OPS.y),3),
+                        "GB%.R" = round(mean(league_totals$`GB%.y`),3),
+                        "LD%.R" = round(mean(league_totals$`LD%.y`),3),
+                        "Hard%.R" = round(mean(league_totals$`Hard%.y`),3),
+                        "BB%.R" = round(mean(league_totals$`BB%.y`),3),
+                        "K%.R" = round(mean(league_totals$`K%.y`),3))
 
